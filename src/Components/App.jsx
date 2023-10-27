0a6fa7c77f91e6fb0137edfdda8b4a22b8fd629d
Shamim <mdshamimreza5552@gmail.com>
2023-10-28 04:35:12 +0600

    basic routing done

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,25 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/home';
+import MovieDetails from '../pages/movieDetails';
+import Movies from '../pages/movies';
+import Popular from '../pages/popular';
+import Search from '../pages/search';
+import TvSeries from '../pages/tvSeries';
 import Layout from './Layout';
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Layout />
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/movies" element={<Movies />} />
+                    <Route path="/popular" element={<Popular />} />
+                    <Route path="/search/:q" element={<Search />} />
+                    <Route path="/tv-shows" element={<TvSeries />} />
+                    <Route path="/movie/:id" element={<MovieDetails />} />
+                </Routes>
+            </Layout>
         </BrowserRouter>
     );
 }
