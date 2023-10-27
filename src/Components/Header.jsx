fe5abce3cabef4b66f930f46180ba0a6cba37a3f
Shamim <mdshamimreza5552@gmail.com>
2023-10-28 04:45:05 +0600

    Changed Link to NavLink

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logoNormal from '../assets/img/logo-transparent.png';
 import logoDarkMode from '../assets/img/logo-white.png';
 
@@ -21,7 +21,7 @@ export default function Header() {
         <header>
             <nav className="bg-white border-gray-200 dark:bg-gray-900 border-b dark:border-b-gray-600">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <Link to="#" className="flex items-center">
+                    <NavLink to="#" className="flex items-center">
                         <img
                             src={logoNormal}
                             className="w-32 block dark:hidden"
@@ -32,7 +32,7 @@ export default function Header() {
                             className="w-32 hidden dark:block"
                             alt="FlixBase Logo"
                         />
-                    </Link>
+                    </NavLink>
 
                     <div className="flex md:order-2">
                         {/* Dark mode Toogle button */}
@@ -176,37 +176,37 @@ export default function Header() {
                         </div>
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <Link
+                                <NavLink
                                     to="/"
-                                    className=" block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                    className=" block py-2 pl-3 pr-4 text-white  bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                                     aria-current="page"
                                 >
                                     Home
-                                </Link>
+                                </NavLink>
                             </li>
                             <li>
-                                <Link
+                                <NavLink
                                     to="/movies"
                                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     Movies
-                                </Link>
+                                </NavLink>
                             </li>
                             <li>
-                                <Link
+                                <NavLink
                                     to="/popular"
                                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     Popular
-                                </Link>
+                                </NavLink>
                             </li>
                             <li>
-                                <Link
+                                <NavLink
                                     to="/tv-shows"
                                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     TV Shows
-                                </Link>
+                                </NavLink>
                             </li>
                         </ul>
                     </div>
